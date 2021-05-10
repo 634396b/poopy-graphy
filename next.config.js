b7a3ceec90dd408b0b67a66c0f42a432399f146d
634396b <69803008+634396b@users.noreply.github.com>
2021-05-09 22:38:04 -0400

    fix: Redirects @ build time

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const moduleExports = {
   },
   productionBrowserSourceMaps: false,
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/whales',
+        permanent: false,
+      },
+      {
+        source: '/whales',
+        destination: '/whales/0x5e90253fbae4dab78aa351f4e6fed08a64ab5590',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 const SentryWebpackPluginOptions = {
