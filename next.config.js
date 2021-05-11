74929d7f6b41576f280c33726f12130827614d0a
634396b <69803008+634396b@users.noreply.github.com>
2021-05-11 18:08:00 -0400

    feat: Whale index for all tokens

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 const moduleExports = {
   poweredByHeader: false,
   productionBrowserSourceMaps: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/whales',
-        permanent: false,
-      },
-      {
-        source: '/whales',
-        destination: '/whales/0x5e90253fbae4dab78aa351f4e6fed08a64ab5590',
-        permanent: false,
-      },
-    ]
-  },
 }
 
 module.exports = moduleExports
---
+++ pages/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { promises as fs } from 'fs'
+import path from 'path'
+
+import Head from 'next/head'
+import type { GetStaticProps } from 'next'
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Paper,
+} from '@material-ui/core'
+import Icon from '@material-ui/icons/Forward'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+  },
+}))
+
+function Tokens(props: any) {
+  const classes = useStyles()
+  return (
+    <>
+      <Head>
+        <title>BSC Whale Tracker</title>
+        <meta
+          name="description"
+          content="Displays sells/buys on BSC tokens from the top wallets"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid item container xs={12} className={classes.root}>
+        <Grid item xs={12}>
+          <Paper>
+            <List component="nav">
+              {props?.tokens?.map((token: any) => {
+                return (
+                  <ListItem
+                    button
+                    component="a"
+                    href={`whales/${token?.tokenAddress}`}
+                  >
+                    <ListItemText>{token?.symbol}</ListItemText>
+                    <ListItemIcon>
+                      <Icon />
+                    </ListItemIcon>
+                  </ListItem>
+                )
+              })}
+            </List>
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const whalesDirectory = path.join(
+    process.cwd(),
+    '.next',
+    'server',
+    'pages',
+    'whales'
+  )
+  const filenames = await fs.readdir(whalesDirectory)
+
+  const tokens = filenames.map(async (filename) => {
+    if (filename.indexOf('.html') > -1 || filename.indexOf('[id]') > -1) {
+      return null
+    } else {
+      const filePath = path.join(whalesDirectory, filename)
+      const fileContents = JSON.parse(await fs.readFile(filePath, 'utf8'))
+      const tokenAddress = filename.replace('.json', '')
+      const symbol = fileContents?.pageProps?.symbol
+      return {
+        tokenAddress,
+        symbol,
+      }
+    }
+  })
+  return {
+    props: {
+      tokens: (await Promise.all(tokens)).filter((t) => t !== null),
+    },
+  }
+}
+
+export default Tokens
