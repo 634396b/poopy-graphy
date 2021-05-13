362a1987c5ad83148135e341fa61d47e5b78841a
634396b <69803008+634396b@users.noreply.github.com>
2021-05-13 18:13:16 -0400

    refactor: Removed style from theme

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,6 +1,4 @@
 import brown from '@material-ui/core/colors/brown'
-import green from '@material-ui/core/colors/green'
-import red from '@material-ui/core/colors/red'
 import { useMemo, useState } from 'react'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import useLocalStorage from 'react-use/lib/useLocalStorage'
@@ -9,8 +7,6 @@ export const palette = {
   primary: {
     main: brown[500],
   },
-  buy: green[500],
-  sell: red[500],
 }
 function useCustomTheme() {
   const [localDarkMode, _setLocalDarkMode, remove] = useLocalStorage('DarkMode')
