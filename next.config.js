93b8d6afd5ccdf65660d25bd51c05c9616924c66
634396b <69803008+634396b@users.noreply.github.com>
2021-05-13 13:36:26 -0400

    fix: Revered buildid

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const moduleExports = {
   poweredByHeader: false,
   productionBrowserSourceMaps: true,
-  generateBuildId: async () => {
-    return 'project'
-  },
 }
 
 module.exports = moduleExports
