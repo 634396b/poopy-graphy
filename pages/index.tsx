50a41995e0a983635b43cbdcd15f73d0b54bbe1c
634396b <69803008+634396b@users.noreply.github.com>
2021-05-13 21:44:18 -0400

    feat: Href pass for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Paper from '@material-ui/core/Paper'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Typography from '@material-ui/core/Typography'
 import makeStyles from '@material-ui/core/styles/makeStyles'
-
+import NextLink from 'next/link'
 import redis from '$/core/redis'
 
 import { useRouter } from 'next/router'
@@ -20,6 +20,7 @@ type HashSymbol = string
 interface TokenHashes {
   [ContractHash: string]: HashSymbol
 }
+
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(2),
@@ -30,9 +31,8 @@ function Tokens({ tokens }: { tokens: TokenHashes }) {
   const classes = useStyles()
   const router = useRouter()
   const [isLoading, setIsLoading] = React.useState(false)
-  const handleClick = (gotoPage: string) => {
+  const handleClick = () => {
     setIsLoading(true)
-    router.push(gotoPage)
   }
   return (
     <>
@@ -54,23 +54,25 @@ function Tokens({ tokens }: { tokens: TokenHashes }) {
                 const whalePage = `/whales/${contractHash}`
                 return (
                   <Grid container alignItems="center" key={contractHash}>
-                    <ListItem button onClick={(_) => handleClick(whalePage)}>
-                      <Grid container item xs alignItems="center">
-                        <Grid item xs={11}>
-                          <Typography component="span" variant="h6">
-                            {symbol}
-                          </Typography>{' '}
+                    <NextLink href={whalePage} passHref>
+                      <ListItem button onClick={handleClick}>
+                        <Grid container item xs alignItems="center">
+                          <Grid item xs={11}>
+                            <Typography component="span" variant="h6">
+                              {symbol}
+                            </Typography>{' '}
+                          </Grid>
+                          <Grid item xs={12}>
+                            <Typography component="span" variant="caption">
+                              {contractHash}
+                            </Typography>
+                          </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                          <Typography component="span" variant="caption">
-                            {contractHash}
-                          </Typography>
+                        <Grid item xs={1}>
+                          <ForwardIcon />
                         </Grid>
-                      </Grid>
-                      <Grid item xs={1}>
-                        <ForwardIcon />
-                      </Grid>
-                    </ListItem>
+                      </ListItem>
+                    </NextLink>
                   </Grid>
                 )
               })}
